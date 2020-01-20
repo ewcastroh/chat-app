f1691c0fcb4a9cd82ccb3f551e3774280556cbe7
ewcastroh <ewcastroh10@gmail.com>
2020-01-20 17:58:08 -0500

    Implemented send and receive messages in chat.

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Client } from '@stomp/stompjs';
 import * as SockJS from 'sockjs-client';
+import { Message } from './models/message';
 
 @Component({
 	selector: 'app-chat',
@@ -11,6 +12,8 @@ export class ChatComponent implements OnInit {
 
 	private client: Client;
 	connected = false;
+	message: Message = new Message();
+	messages: Message[] = [];
 
 	constructor() { }
 
@@ -24,6 +27,14 @@ export class ChatComponent implements OnInit {
 		this.client.onConnect = (frame) => {
 			console.log('Connected: ' + this.client.connected + ' : ' + frame);
 			this.connected = true;
+
+			this.client.subscribe('/chat/message', e => {
+				const message: Message = JSON.parse(e.body) as Message;
+				message.date = new Date(message.date);
+				this.messages.push(message);
+				console.log('Received message: ' + JSON.stringify(message));
+
+			});
 		};
 
 		this.client.onDisconnect = (frame) => {
@@ -38,6 +49,11 @@ export class ChatComponent implements OnInit {
 
 	disconnect(): void {
 		this.client.deactivate();
+	}
+
+	sendMessage(): void {
+		this.client.publish({ destination: '/app/message', body: JSON.stringify(this.message)});
+		this.message.text = '';
 	}
 
 }
